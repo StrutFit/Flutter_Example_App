4afd04af3690d429272d3d0b8d62e8610a7edb7a
Jake Thomas <jake.strutfit@outlook.com>
2024-12-17 12:31:38 +1300

    refactor

--- android/app/src/main/kotlin/com/example/flutter_example_app/StrutFitButtonManager.kt
+++ android/app/src/main/kotlin/com/example/flutter_example_app/StrutFitButtonManager.kt
@@ -26,38 +26,38 @@ class StrutFitButtonManager(
             .registry
             .registerViewFactory(
                 "strutfit_button_view",
-                StrutFitButtonViewFactory(activity) // Pass activity directly
+                StrutFitButtonPlatformViewFactory(activity) // Pass activity directly
             )
         
         Log.d("StrutFitButtonManager", "✅ StrutFit button view registered successfully")
     }
 
     // Platform View Factory
-    class StrutFitButtonViewFactory(private val activity: Activity) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    class StrutFitButtonPlatformViewFactory(private val activity: Activity) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
         override fun create(context: Context?, id: Int, args: Any?): PlatformView {
             val creationParams = args as? Map<String, Any>
             Log.d("StrutFitButtonViewFactory", "🔥 Arguments received: $creationParams")
-            return StrutFitButtonView(context!!, activity, creationParams)
+            return StrutFitButtonPlatformView(context!!, activity, creationParams)
         }
     }
 
     // Platform View
-    class StrutFitButtonView(
+    class StrutFitButtonPlatformView(
         private val context: Context,
         private val activity: Activity,
         private val creationParams: Map<String, Any>?
     ) : PlatformView {
 
-        private val strutFitButtonView: strutfit.button.StrutFitButtonView = strutfit.button.StrutFitButtonView(context)
+        private val strutFitButtonView: StrutFitButtonView = StrutFitButtonView(context)
 
         init {
             val organizationId = creationParams?.get("organizationId") as? Int ?: 0
             val productCode = creationParams?.get("productCode") as? String ?: ""
-            val sizeUnit = creationParams?.get("sizeUnit") as? String ?: ""
-            val apparelSizeUnit = creationParams?.get("apparelSizeUnit") as? String ?: ""
+            val sizeUnit = creationParams?.get("sizeUnit") as? String
+            val apparelSizeUnit = creationParams?.get("apparelSizeUnit") as? String
 
             if (organizationId == 0 || productCode.isEmpty()) {
-                Log.e("StrutFitButtonView", "❌ Missing required parameters for StrutFit Button.")
+                Log.e("StrutFitButtonPlatformView", "❌ Missing required parameters for StrutFit Button.")
             } else {
                 strutFitButtonView.id = View.generateViewId()
                 strutFitButtonView.visibility = View.GONE
--- android/app/src/main/kotlin/com/example/flutter_example_app/StrutFitTrackingManager.kt
+++ android/app/src/main/kotlin/com/example/flutter_example_app/StrutFitTrackingManager.kt
@@ -8,7 +8,7 @@ import strutfit.button.models.ConversionItem
 import strutfit.button.StrutFitTracking
 
 class StrutFitTrackingManager(
-    private val activity: Activity, 
+    activity: Activity,
     flutterEngine: FlutterEngine
 ) {
 
@@ -35,4 +35,38 @@ class StrutFitTrackingManager(
 
         Log.d("StrutFitTrackingManager", "✅ Tracking pixel channel registered successfully")
     }
+
+    class StrutFitTrackingHandler(private val activity: Activity) {
+
+        fun registerOrderForStrutFit(args: Map<String, Any>, result: MethodChannel.Result) {
+            try {
+                val organizationId = args["organizationId"] as Int
+                val orderReference = args["orderReference"] as String
+                val orderValue = args["orderValue"] as Double
+                val currencyCode = args["currencyCode"] as String
+                val userEmail = args["userEmail"] as String
+
+                val strutFitTracking = StrutFitTracking(activity, organizationId)
+                val items = ArrayList<ConversionItem>()
+
+                val productItems = args["items"] as List<Map<String, Any>>
+                for (item in productItems) {
+                    val productIdentifier = item["productIdentifier"] as String
+                    val price = item["price"] as Double
+                    val quantity = item["quantity"] as Int
+                    val size = item["size"] as String
+                    val conversionItem = ConversionItem(productIdentifier, price, quantity, size)
+                    items.add(conversionItem)
+                }
+
+                strutFitTracking.registerOrder(orderReference, orderValue, currencyCode, items, userEmail)
+                Log.d("StrutFitTracking", "✅ Order Tracked: $orderReference")
+                result.success("Order Tracked Successfully")
+            } catch (e: Exception) {
+                Log.e("StrutFitTracking", "❌ Error tracking order: ${e.message}", e)
+                result.error("TRACKING_ERROR", e.message, null)
+            }
+        }
+    }
 }
+
